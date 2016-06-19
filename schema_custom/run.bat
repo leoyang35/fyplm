a991286742ddcb859ee501ba5f676acb6aeecbad
leo.yang <ycj3541@icloud.com>
2016-06-19 20:47:32 +0800

    Update and merge properties from latest FY SVN

--- schema_custom/run.bat
+++ schema_custom/run.bat
@@ -2,5 +2,6 @@
 @echo off
 mql -b MATRIX-R-local -c "set context user creator;run preSpinner.tcl;exec prog emxSpinnerAgent.tcl;run postSpinner.tcl;"
 xcopy "..\enovia_custom\WEB-INF\classes\*.properties" "C:\apache-tomee-plus-1.6.0\webapps\enovia\WEB-INF\classes" /y /q
+xcopy "..\enovia_custom\engineeringcentral\*.jsp" "C:\apache-tomee-plus-1.6.0\webapps\enovia\engineeringcentral" /y /q
 pause
 goto A
