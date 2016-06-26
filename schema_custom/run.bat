45d5d2ffedd9b73b8f11001f63b2eecf54eb4a7e
leo.yang <ycj3541@icloud.com>
2016-06-26 20:09:22 +0800

    1. Product Spec Technical Requirement 2. Fix Create BBXHQD form issue

--- schema_custom/run.bat
+++ schema_custom/run.bat
@@ -3,5 +3,6 @@
 mql -b MATRIX-R-local -c "set context user creator;run preSpinner.tcl;exec prog emxSpinnerAgent.tcl;run postSpinner.tcl;"
 xcopy "..\enovia_custom\WEB-INF\classes\*.properties" "C:\apache-tomee-plus-1.6.0\webapps\enovia\WEB-INF\classes" /y /q
 xcopy "..\enovia_custom\engineeringcentral\*.jsp" "C:\apache-tomee-plus-1.6.0\webapps\enovia\engineeringcentral" /y /q
+xcopy "..\enovia_custom\components\*" "C:\apache-tomee-plus-1.6.0\webapps\enovia\components" /y /q
 pause
 goto A
